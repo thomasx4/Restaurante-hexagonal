11106e7b13bfcc9934256f9c71ec6499dbc2d44c
thomasx4 <castanedamunoz23@gmail.com>
2026-03-13 20:42:06 -0500

    feat: Category.java model TD

--- src/main/java/com/restaurante/hexagonal/domain/model/Category.java
+++ src/main/java/com/restaurante/hexagonal/domain/model/Category.java
@@ -1,1 +1,12 @@
 package  com.restaurante.hexagonal.domain.model;
+
+import lombok.Data;
+
+@Data
+public class Category {
+
+    private Long id;
+    private String name;
+    private String description;
+
+}
