575329058ba8b7fcae683d71a4da0722583ef15c
thomasx4 <castanedamunoz23@gmail.com>
2026-03-16 06:37:05 -0500

    feat: Product.java model TD

--- src/main/java/com/restaurante/hexagonal/domain/model/Product.java
+++ src/main/java/com/restaurante/hexagonal/domain/model/Product.java
@@ -1,5 +1,13 @@
 package com.restaurante.hexagonal.domain.model;
 
+import lombok.Data;
+
+@Data
 public class Product {
-    
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Boolean available;
+    private Long categoryId;
 }
