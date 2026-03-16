a83538c4b49121a0a8df5ecbf34451103fd7dd56
thomasx4 <castanedamunoz23@gmail.com>
2026-03-16 14:47:04 -0500

    feat: PaymentResponseDTO and PaymentRequestDTO DTO TD

--- src/main/java/com/restaurante/hexagonal/application/dto/PaymentRequestDTO.java
+++ src/main/java/com/restaurante/hexagonal/application/dto/PaymentRequestDTO.java
@@ -1,5 +1,13 @@
 package com.restaurante.hexagonal.application.dto;
 
+import lombok.Data;
+
+@Data
 public class PaymentRequestDTO {
     
+    private Long order_id;
+    private Float amount;
+    private String payment_method;
+    private String status;
+
 }
