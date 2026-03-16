04fb9c2ba64072523fc5e3486ae00d5f4a2dc588
thomasx4 <castanedamunoz23@gmail.com>
2026-03-16 15:52:02 -0500

    feat: PaymentJpaRepository JPA TD

--- src/main/java/com/restaurante/hexagonal/infrastructure/persistence/jpa/PaymentJpaRepository.java
+++ src/main/java/com/restaurante/hexagonal/infrastructure/persistence/jpa/PaymentJpaRepository.java
@@ -1,5 +1,17 @@
 package com.restaurante.hexagonal.infrastructure.persistence.jpa;
 
-public class PaymentJpaRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.restaurante.hexagonal.infrastructure.persistence.entity.PaymentEntity;
+
+@Repository
+public interface PaymentJpaRepository extends JpaRepository<PaymentEntity, Long>{
+
+    // Buscar pagos por id de orden
+    List<PaymentEntity> findByOrderId(Long orderId);
 }
+
+
